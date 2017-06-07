105c6eae0c08c4ba4a6ea4ced94a1acdaac79c28
yoyuse <yoyuse@gmail.com>
2017-06-07 12:57:24 +0900

    Add test: go-ttt_test.go, ttt/decode_test.go

---
+++ go-ttt_test.go
@@ -0,0 +1,12 @@
+// 2017-06-07
+
+package main
+
+import (
+)
+
+func ExampleDecode_string() {
+	do_ttt([]string{"kdjd;sjdkd;s", "lgkg;gjslghdhghdkshdkd"}, false, true)
+	// Output:
+	// の、が、のが なにをしないでいたいの
+}
---
+++ ttt/decode_test.go
@@ -0,0 +1,53 @@
+// 2017-06-07
+
+package ttt
+
+import (
+	"testing"
+)
+
+type stringTest struct {
+	in, out string
+}
+
+var Tests1 = []stringTest {
+	{"kdjd;sjdkd;s", "の、が、のが"},
+	{"ABCDyfhdyd", "ABCDあいう"},
+	{"abcd yfhdyd", "abcd あいう"},
+	{"abcd:yfhdyd", "abcdあいう"},
+	{"ysksjsks/ajgjfkdt8p3;gpzjshdjtighd", "わたしたちは氷砂糖をほしいくらい"},
+	{"あの Iha-Tovo kd,fhrjaoajrks風", "あの Iha-Tovo のすきとおった風"},
+	{"うつくしい森で飾られた Morio:/v", "うつくしい森で飾られた Morio市"},
+	{"(またアラッディン　\ruby{lyfjlk}{ラムプ}とり)", "(またアラッディン　\ruby{洋燈}{ラムプ}とり)"},
+	{";d;fha", "123"},
+}
+
+func TestDecode_substring(t *testing.T) {
+	for _, ts := range Tests1 {
+		dec := Decode_substring(ts.in)
+		if dec != ts.out {
+			t.Errorf("Decode_substring(%q) == %q, want %q", ts.in, dec, ts.out)
+		}
+	}
+}
+
+var Tests2 = []stringTest {
+	{"kdjd;sjdkd;s\x1bj", "の、が、のが"},
+	{"ABCDyfhdyd\x1bj", "ABCDあいう"},
+	{"abcd yfhdyd\x1bj", "abcd あいう"},
+	{"abcd:yfhdyd\x1bj", "abcdあいう"},
+	{"ysksjsks/ajgjfkdt8p3;gpzjshdjtighd\x1bj", "わたしたちは氷砂糖をほしいくらい"},
+	{"yfkd\x1bj Iha-Tovo kd,fhrjaoajrks風\x1bj", "あの Iha-Tovo のすきとおった風"},
+	{"うつくしい森で飾られた Morio:/v\x1bj", "うつくしい森で飾られた Morio市"},
+	{"(またアラッディン　\ruby{lyfjlk}{usubmw}jajc)\x1bj\x1bj\x1bj", "(またアラッディン　\ruby{洋燈}{ラムプ}とり)"},
+	{";d\x1bj;f\x1bjha", "岳ha"},
+}
+
+func TestDecode_xfer(t *testing.T) {
+	for _, ts := range Tests2 {
+		dec := Decode_xfer(ts.in)
+		if dec != ts.out {
+			t.Errorf("Decode_xfer(%q) == %q, want %q", ts.in, dec, ts.out)
+		}
+	}
+}
